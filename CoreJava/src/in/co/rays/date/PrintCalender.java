33a8830757bb2ed4852e990e4128b1eca07772d6
Richa1803 <richasinghyas@gmail.com>
2026-08-17 10:53:50 +0530

    ok

--- CoreJava/src/in/co/rays/date/PrintCalender.java
+++ CoreJava/src/in/co/rays/date/PrintCalender.java
@@ -1,9 +1,23 @@
 package in.co.rays.date;
 
+import java.util.Date;
+
 public class PrintCalender {
 	public static void main(String[] args)
 	{
+		Date date = new Date();
 		
+		System.out.println("Calendar Date :"+date);
+		System.out.println("Calendar Year 2027");
+		
+		for(int month=1; month<=12; month++) {
+			System.out.println("Month :"+month);
+			for(int day=1; day<=31; day++)
+			{
+				System.out.print(day+" ");
+			}
+			System.out.println();
+		}
 	}
 
 }
